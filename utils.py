8a2c0f9f5807aa6352aa9bf4eabdde400f3bda3e
RDoerfel <doerfelruben@aol.com>
2024-10-05 13:40:20 +0200

    add setup function for logging to utils

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torchvision
 from PIL import Image
@@ -44,3 +45,10 @@ def transform_sampled_image(image):
     image = (image.clamp(-1, 1) + 1) / 2  # rescale to [0, 1]
     image = (image * 255).type(torch.uint8)  # rescale to [0, 255]
     return image
+
+
+def setup_logging_dirs(run_name):
+    os.makedirs("models", exist_ok=True)
+    os.makedirs("results", exist_ok=True)
+    os.makedirs(os.path.join("models", run_name), exist_ok=True)
+    os.makedirs(os.path.join("results", run_name), exist_ok=True)
